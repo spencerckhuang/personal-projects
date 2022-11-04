56f7ebe9ba02fbbc3527a201724446da7ddf09f4
spencerckhuang <shuan126@jh.edu>
2022-11-04 13:30:18 -0400

    updated map of gradevalues and added new methods

--- gpa_calculator/gpa_functions.cpp
+++ gpa_calculator/gpa_functions.cpp
@@ -1,39 +1,42 @@
 #include "gpa_functions.h"
 #include <iostream>
+#include <string>
+#include <vector>
 
 using std::cout; using std::endl; using std::cerr;
+using std::string; using std::vector;
 
-std::map<std::string, double> gradeValues = {
-    {"A+", 4.0}, {"A", 4.0}, {"A-", 3.7},
-    {"B+", 3.3}, {"B", 3.0}, {"B-", 2.7},
-    {"C+", 2.3}, {"C", 2.0}, {"C-", 1.7},
-    {"D+", 1.3}, {"D", 1.0}, {"F", 0.0},
-}
+
 
 Course::Course(string cn, int c, string g): course_name(cn){
-    /* make sure credit # is valid */
+    setCredits(c);
+    setGrade(g);
+}
+
+void Course::setCredits(int c){
     if(c < 1){
-        cerr << "Invalid credits entered for course " << course_name << ". Changing to 1 by default." << endl;
+        cerr << "Invalid credits entered in setCredits for course " << course_name << ". Changing to 1 by default." << endl;
         credits = 1;
     } else {
         credits = c;
     }
-
-    /* make sure grade is valid */
-    if(gradeValues.find(g) != gradeValues.end()){
-        grade = g;
-    } else {
-        cerr << "Invalid grade entered for " << course_name << ". Changing to C by default." << endl;
-        grade = "C";
-    }
-
-    GPA = 0.0;
-}
-
-void Course::setCredits(int i){
     return;
 }
 
 void Course::setGrade(string g){
+    if(gradeValues.find(g) != gradeValues.end()){
+        grade = g;
+    } else {
+        cerr << "Invalid grade entered in setGrade for " << course_name << ". Changing to C by default." << endl;
+        grade = "C";
+    }
     return;
+}
+
+bool deansList(double g, int c){
+    return(g >= 3.5 && c >= 14);
+}
+
+double calculateGPA(vector<Course> courses){
+    return 0.0;
 }
